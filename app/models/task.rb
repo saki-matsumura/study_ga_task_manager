ed540308db331d8a4ed87ca9b30767077b947671
“MatsumuraSaki” <massaki3156@gmail.com>
2023-12-17 15:18:18 +0900

    タスクが削除された時、関連する工程も削除するよう修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,16 +1,10 @@
 class Task < ApplicationRecord
   # clientとworking_processはnilでもOK
   belongs_to :client, optional: true
-  accepts_nested_attributes_for :client,
-    reject_if: :all_blank
-
   belongs_to :user
 
   # タスクが削除されたら、付随する工程も削除する
-  has_many :working_processes
-  accepts_nested_attributes_for :working_processes,
-    allow_destroy: true,
-    reject_if: :all_blank
+  has_many :working_processes, dependent: :destroy
   
   validates :title, presence: true
   validates :note, length: { maximum: 140 }
