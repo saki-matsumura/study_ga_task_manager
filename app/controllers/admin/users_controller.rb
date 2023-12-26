d22870ee55d5dfaa04fbc38b00d3d752452cbf03
“MatsumuraSaki” <massaki3156@gmail.com>
2023-12-26 17:37:01 +0900

    管理者以外がユーザー一覧にアクセスすると、カレンダーページに遷移するよう修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,13 @@
 class Admin::UsersController < ApplicationController
+  before_action :exclude_general
+
   def index
     @users = User.all
+  end
+
+  private
+
+  def exclude_general
+    redirect_to calendar_tasks_path, notice: "管理者以外はアクセスできません" unless current_user.roll == 'admin'
   end
 end
